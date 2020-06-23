8ddd14ca64d9549a63f170a48906347ff277a679
ubryce <bknguyen@ualberta.ca>
2020-06-22 20:11:46 -0600

    react front end setup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
 
+const path = require('path');
+
 // load config
 dotenv.config({ path: './config/config.env' })
 
@@ -15,6 +17,20 @@ if(process.env.NODE_ENV == 'development'){
     app.use(morgan('dev'))
 }
 
-const PORT = process.env.PORT || 3000
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
