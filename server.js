f93173f2d5bd0d2610bdd14104fc2a1019d3db75
ubryce <bknguyen@ualberta.ca>
2020-06-22 19:45:27 -0600

    setup morgan for logging in

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 // load config
 dotenv.config({ path: './config/config.env' })
 
+connectDB()
+
 const app = express()
+
+// check if in dev mode
+if(process.env.NODE_ENV == 'development'){
+    app.use(morgan('dev'))
+}
 
 const PORT = process.env.PORT || 3000
 
