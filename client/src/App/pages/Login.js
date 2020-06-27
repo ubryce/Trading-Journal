97783a958e7b22b4d8c7485003ce18b90926221f
ubryce <bknguyen@ualberta.ca>
2020-06-27 16:03:27 -0600

    add css for login

--- client/src/App/pages/Login.js
+++ client/src/App/pages/Login.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import './css/login.css';
 
 class Login extends Component {
   render() {
     return (
     <div className="App">
       <h1>Login</h1>
-    
+      
 
 
 
